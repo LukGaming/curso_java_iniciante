f0a94148a72fea8b606965b398ae455faf741d51
Paulo <thelukgaming333@gmail.com>
2025-12-18 22:01:36 -0300

    Primitivos parte 01

---
+++ src/primitivos/Primitivos01.java
@@ -0,0 +1,25 @@
+package primitivos;
+
+public class Primitivos01 {
+    void main(){
+        byte idade = 27;
+
+        IO.println("Idade: " + idade);
+
+        byte temperatura = -10;
+
+        IO.println("Temperatura: " + temperatura);
+
+        byte valor = 127;
+
+        short ano = 2025;
+
+        IO.println("Ano: "+ ano);
+
+        short temperaturaMinima = -273;
+
+        IO.print("Temperatura mínima em Graus: "+ temperaturaMinima);
+
+        int quantidade = 32768;
+    }
+}
