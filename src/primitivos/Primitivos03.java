d1fee131cc7eeed88c48b8f78bd988fae3a979c8
Paulo <thelukgaming333@gmail.com>
2025-12-19 22:33:51 -0300

    Primitivos parte 03

---
+++ src/primitivos/Primitivos03.java
@@ -0,0 +1,68 @@
+package primitivos;
+
+public class Primitivos03 {
+    void main(){
+        float preco = 9.99F;
+
+        IO.println("Preço R$"+ preco);
+
+        float altura = 1.83F;
+
+        IO.println("Altura: "+ altura);
+
+        float nota = 65.5F;
+
+        IO.println("Nota: "+ nota);
+
+        float a = 5.5F;
+        float b = 2.3F;
+
+        float soma = a + b;
+        float divisao = a / b;
+        float subtracao = a - b;
+        float multiplicacao = a * b;
+
+        IO.println("Soma: "+ soma);
+        IO.println("Divisao: "+ divisao);
+        IO.println("Subtracao: "+ subtracao);
+        IO.println("Multiplicacao: "+ multiplicacao);
+
+        float precisao = 0.1F * 0.2F;
+
+        IO.println("Precisão: "+ precisao);
+
+        double salario = 3500.75;
+
+        IO.println("Salário R$"+ 3500.75);
+
+        double altura2 = 1.83;
+
+        IO.println("Altura2: "+ altura2);
+
+        double temperatura = -10.5;
+
+        IO.println("Temperatura: "+ -10.5 + " graus celsius");
+
+        double nota2 = 9.75;
+
+        IO.println("Nota: "+ nota2);
+
+        float c = 5.5F;
+        float d = 2.3F;
+
+        float soma1 = c + d;
+        float divisao1 = c / d;
+        float subtracao1 = c - d;
+        float multiplicacao1 = c * d;
+
+        IO.println("Soma: "+ soma1);
+        IO.println("Divisao: "+ divisao1);
+        IO.println("Subtracao: "+ subtracao1);
+        IO.println("Multiplicacao: "+ multiplicacao1);
+
+        double precisao2 = 0.1 * 0.2;
+
+        IO.println("Precisão 2: "+ precisao2);
+
+    }
+}
